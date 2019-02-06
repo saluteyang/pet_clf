8b2e6489b19cae77fad6563244c18bea31c81087
David Yang <saluteyang@gmail.com>
2019-02-06 15:50:13 -0500

    Added visualizations and annotations

--- pet_clf.py
+++ pet_clf.py
@@ -22,7 +22,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
+sns.set()
 np.set_printoptions(suppress=True) # Suppress scientific notation where possible
 
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
@@ -33,6 +34,8 @@ from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.metrics import cohen_kappa_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from mlxtend.plotting import plot_decision_regions
+from sklearn.tree import export_graphviz
 
 # data pre-processing #########################
 # training data
@@ -52,31 +55,31 @@ train_df2 = pd.get_dummies(train_df, columns=['Type', 'Breed1', 'Breed2', 'Gende
                                               'Sterilized', 'Health', 'State'])
 
 # test train split
-X_train, X_test, label_train, label_test = train_test_split(train_df2.drop(columns=['AdoptionSpeed', 'PetID']),
+X2_train, X2_test, label_train, label_test = train_test_split(train_df2.drop(columns=['AdoptionSpeed', 'PetID']),
                                                             train_df2['AdoptionSpeed'],
                                                             test_size=0.3, random_state=41)
 
 # standard scaler (omit for random forest)
 std = StandardScaler()
-std.fit(X_train)
-X_train = std.transform(X_train)
-X_test = std.transform(X_test)
+std.fit(X2_train)
+X2_train = std.transform(X2_train)
+X2_test = std.transform(X2_test)
 
 # model fitting ###########################
 # logistic CV with f1_score scoring
-logitCV = LogisticRegressionCV(cv=5, random_state=0, scoring='f1_macro').fit(X_train, label_train)
-logitCV.score(X_train, label_train)   # 0.77 (acc); 0.636 (acc, f1_macro scoring)
-logitCV.score(X_test, label_test)   # 0.74 (acc); 0.598 (acc, f1_macro scoring)
-
-f1_score(label_train, logitCV.predict(X_train))   # 0.41
-f1_score(label_test, logitCV.predict(X_test))   # 0.36
+# logitCV = LogisticRegressionCV(cv=5, random_state=0, scoring='f1_macro').fit(X2_train, label_train)
+# logitCV.score(X2_train, label_train)   # 0.77 (acc); 0.636 (acc, f1_macro scoring)
+# logitCV.score(X2_test, label_test)   # 0.74 (acc); 0.598 (acc, f1_macro scoring)
+#
+# f1_score(label_train, logitCV.predict(X2_train))   # 0.41
+# f1_score(label_test, logitCV.predict(X2_test))   # 0.36
 
 # logistic with manually set regularization
 logit = LogisticRegression(C=0.95)
-logit.fit(X_train, label_train)
+logit.fit(X2_train, label_train)
 print("The score for logistic regression is")
-print("Training: {:.2f}%".format(100*f1_score(label_train, logit.predict(X_train))))
-print("Test set: {:.2f}%".format(100*f1_score(label_test, logit.predict(X_test))))
+print("Training: {:.2f}%".format(100*f1_score(label_train, logit.predict(X2_train))))
+print("Test set: {:.2f}%".format(100*f1_score(label_test, logit.predict(X2_test))))
 
 # interpretation and diagnostics ###################
 # select numerical columns for pairplot
@@ -91,15 +94,64 @@ plot_features(train_df)
 
 # show coefs for logistic regression with feature names
 feature_names = train_df2.drop(columns=['AdoptionSpeed', 'PetID']).columns
-feature_coefs = pd.DataFrame({'feature': feature_names, 'coef': logitCV.coef_[0]})
+feature_coefs = pd.DataFrame({'feature': feature_names, 'coef': logit.coef_[0]})
 feature_coefs['abs_coef'] = abs(feature_coefs['coef'])
 feature_coefs.sort_values(by='abs_coef', ascending=False)
 
+#           feature      coef  abs_coef
+# 173    Breed1_307  0.601116  0.601116
+# 0             Age  0.394190  0.394190
+# 146    Breed1_276 -0.317996  0.317996  (Maine Coon, largest domestic breed)
+# 100    Breed1_207 -0.313454  0.313454
+# 4        PhotoAmt -0.294131  0.294131
+# 8          Type_2  0.276278  0.276278
+# 7          Type_1 -0.276278  0.276278
+# 80     Breed1_169 -0.261142  0.261142
+# 99     Breed1_206 -0.249684  0.249684
+# 330  Sterilized_2 -0.242027  0.242027
+
+# ad hoc exploratory analysis  #############
+# breed count
+train_df.groupby(['Type', 'Breed1'])[['Breed1']].count().sort_values(by='Breed1', ascending=False)[:5]
+
+# 1    307       4610  mixed breed dog
+# 2    266       2463  domestic short hair cat
+
+# photo count histogram
+grouped = train_df.groupby(['PhotoAmt'])['Type'].count().sort_values(ascending=False)
+grouped.head()
+train_df['PhotoAmt'].hist()
+plt.show()
+
+# Plot decision region (optional) ###########################
+# plot_df2 = train_df2[['Age', 'PhotoAmt', 'AdoptionSpeed']]
+#
+# X_train_plot, X_test, label_train, label_test = train_test_split(plot_df2.drop(columns=['AdoptionSpeed']),
+#                                                             plot_df2['AdoptionSpeed'],
+#                                                             test_size=0.3, random_state=41)
+#
+#
+# std = StandardScaler()
+# std.fit(X_train_plot)
+# X_train_plot = std.transform(X_train_plot)
+#
+# logit_plot = LogisticRegression(C=0.95)
+# logit_plot.fit(X_train_plot, label_train)
+#
+# plot_decision_regions(X=X_train_plot,
+#                       y=label_train.values,
+#                       clf=logit_plot,
+#                       legend=2,
+#                       markers="o")
+# plt.gcf().set_size_inches(12, 10)
+# plt.show()
+
+######################################
 # how did the language score perform?
 feature_coefs[feature_coefs['feature'].isin(['magnitude', 'score'])]
 
 # Print confusion matrix for logistic regression
-logit_confusion = confusion_matrix(label_test, logit.predict(X_test))
+logit_confusion = confusion_matrix(label_test, logit.predict(X2_test))
 plt.figure(dpi=150)
 sns.heatmap(logit_confusion, cmap=plt.cm.Blues, annot=True, square=True,
            xticklabels=['adopted', 'not adopted'],
@@ -126,7 +178,7 @@ plt.show()
 # false positive rate = FP / true negatives = FP / (FP + TN)
 
 # ROC AUC
-fpr, tpr, thresholds = roc_curve(label_test, logit.predict_proba(X_test)[:, 1])
+fpr, tpr, thresholds = roc_curve(label_test, logit.predict_proba(X2_test)[:, 1])
 
 plt.plot(fpr, tpr,lw=2)
 plt.plot([0,1],[0,1],c='violet',ls='--')
@@ -137,7 +189,7 @@ plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.title('ROC curve for pet adoption')
 plt.show()
-print("ROC AUC score = ", roc_auc_score(label_test, logit.predict_proba(X_test)[:, 1]))
+print("ROC AUC score = ", roc_auc_score(label_test, logit.predict_proba(X2_test)[:, 1]))
 
 # random forest fitting #######################
 
@@ -170,6 +222,14 @@ print("Test set: {:.2f}%".format(100*f1_score(label_test, rfmodel2.predict(X3_te
 # Training: 69.54%
 # Test set: 56.55%
 
+# plot first few levels of decision tree  ###############
+# extract one single tree
+estimator = rfmodel2.estimators_[5]
+export_graphviz(estimator, out_file='tree.dot', max_depth=3,
+                feature_names=train_df.drop(columns=['AdoptionSpeed', 'PetID']).columns,
+                class_names=['adopted', 'not_adopted'],
+                rounded=True, proportion=False,
+                precision=2, filled=True)
 
 # knn fitting ########################
 knn_f1 = []
@@ -235,4 +295,4 @@ print("The score for logistic regression is")
 print("Training: {:.2f}%".format(100*f1_score(label_train, logit2.predict(X2_train), average='weighted')))
 print("Test set: {:.2f}%".format(100*f1_score(label_test, logit2.predict(X2_test), average='weighted')))
 
-cohen_kappa_score(label_test, logit2.predict(X_test))
+cohen_kappa_score(label_test, logit2.predict(X2_test))
